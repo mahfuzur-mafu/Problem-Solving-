4483dfa5c9c6eb75d6f734397c73ba3646a4fb2e
Md Mahfuzur Rahman <63856744+mahfuzur-mafu@users.noreply.github.com>
2020-09-21 01:16:01 +0600

    Add files via upload

---
+++ HackerRank Solution/Cpp Solution/Pointer.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+void update(int *a,int *b) {
+    // Complete this function
+     int x,y;
+    x = *a + *b;
+    y = *a - *b;
+    *a = x;
+    *b =abs(y);
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
+
