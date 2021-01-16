b447286b89ea97ea71181fb60e7f89c02c4a5de1
Md Mahfuzur Rahman <63856744+mahfuzur-mafu@users.noreply.github.com>
2021-01-16 11:59:59 +0600

    Add files via upload

---
+++ Timus Online Judge/1083. Factorials!!!.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    string str;
+    cin>>n;
+    cin>>str;
+    int len = str.size();
+
+    unsigned  long long sum=1;
+    for(int i = n ; i > 0 ; i=i-len)
+    {
+        sum=sum*i;
+    }
+    cout<<sum<<endl;
+
+    return 0;
+}
