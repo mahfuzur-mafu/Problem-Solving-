091e2317060683f1518b5ae8bbcf7a593d4c7a91
Md Mahfuzur Rahman <63856744+mahfuzur-mafu@users.noreply.github.com>
2021-01-16 11:02:21 +0600

    Add files via upload

---
+++ LightOj Solution/Parity.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int t,i,n;
+    scanf("%d", &t);
+    for (i=1;i<=t;i++)
+    {
+        scanf("%d",&n);
+        int count=0;
+        while(n)
+        {
+            count=count+n%2;
+            n=n/2;
+        }
+        if(count % 2)
+        {
+            printf("Case %d: odd\n", i);
+        }
+
+        else
+        {
+            printf("Case %d: even\n", i);
+
+        }
+    }
+}
