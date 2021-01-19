5d773ead608ad435c814e65910ab17c36439aee9
Md Mahfuzur Rahman <63856744+mahfuzur-mafu@users.noreply.github.com>
2021-01-19 10:52:46 +0600

    Add files via upload

---
+++ Some Useful  Program/Array index Even Odd.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,j=0;
+    scanf("%d",&n);
+    int arr[n];
+
+       for(i=1;i<=n;i++)
+    {
+       scanf("%d",&arr[i]);
+    }
+
+        for(i=1;i<=n;i++)
+    {
+        if(i%2!=0)
+        {
+            arr[j]=i;
+            j++;
+        }
+    }
+
+    for(i=1;i<=n;i++)
+    {
+        if(i%2==0)
+        {
+            arr[j]=i;
+            j++;
+        }
+    }
+
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+
+    return 0;
+}
+
+
