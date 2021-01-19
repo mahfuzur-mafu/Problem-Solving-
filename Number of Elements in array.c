f812f66f3493029dcd33cb8bbcfd0348d34a7fd4
Md Mahfuzur Rahman <63856744+mahfuzur-mafu@users.noreply.github.com>
2021-01-19 10:53:04 +0600

    Add files via upload

---
+++ Number of Elements in array.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main()
+{
+    int arr[100], freq[100];
+    int sz, i, j, cnt;
+    scanf("%d", &sz);
+
+    for(i=0; i<sz; i++)
+    {
+        scanf("%d", &arr[i]);
+        freq[i] = -2;
+    }
+
+    for(i=0; i<sz; i++)
+    {
+        cnt = 1;
+        for(j=i+1; j<sz; j++){
+            if(arr[i]==arr[j])
+            {
+                cnt++;
+
+
+                freq[j] = 0;
+            }
+        }
+
+
+
+        if(freq[i] != 0)
+        {
+            freq[i] = cnt;
+        }
+
+
+    }
+    for(i=0; i<sz; i++)
+    {
+        if(freq[i] != 0)
+        {
+            printf("%d %d\n", arr[i], freq[i]);
+        }
+    }
+}
+
