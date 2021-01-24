4f6c9cab432ec042065f89f9573ec37d428b2759
Md Mahfuzur Rahman <63856744+mahfuzur-mafu@users.noreply.github.com>
2021-01-24 16:58:36 +0600

    Add files via upload

---
+++ LightOj Solution/Brush (I).c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a,j;
+    int sum=0;
+    for(int i=1; i<=n; i++)
+    {
+        int m;
+        scanf("%d",&m);
+        for( j=1,sum=0; j<=m; j++)
+        {
+
+            scanf("%d",&a);
+            if(a>0)   sum=sum+a;
+
+        }
+        if(sum>=0)
+        printf("Case %d: %d\n",i,sum);
+
+    }
+    return 0;
+}
