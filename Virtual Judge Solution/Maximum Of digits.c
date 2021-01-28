0c70211294c196e6dc40d8eceef152cecf477aae
Md Mahfuzur Rahman <63856744+mahfuzur-mafu@users.noreply.github.com>
2021-01-28 19:42:20 +0600

    Add files via upload

---
+++ Virtual Judge Solution/Maximum Of digits.c
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n,i,a[10]={4,7,47,74,447,474,477,774,747,744},cnt=0;
+    scanf("%d",&n);
+    for(i=0;i<10;i++){
+    if(n==a[i]||n%a[i]==0){
+        printf("YES\n");
+        break;
+    }
+
+    else cnt++;
+    }
+    if(cnt==10) printf("NO\n");
+    return 0;
+}
