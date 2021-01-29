a88700f30709743ad243c9ead100e529f6be7d51
Md Mahfuzur Rahman <63856744+mahfuzur-mafu@users.noreply.github.com>
2021-01-29 15:31:32 +0600

    Add files via upload

---
+++ Virtual Judge Solution/Parking.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <bits/stdc++.h>
+
+int main()
+{
+    int i,n,temp;
+    scanf("%d",&n);
+
+  while(n--)
+    {
+        int num, last = 99, first = 0;
+        scanf("%d",&num);
+
+        for ( i = 0; i < num; ++i)
+        {
+            scanf("%d", &temp);
+
+            if (temp < last)
+            {
+                last = temp;
+            }
+            if (temp > first)
+            {
+                first = temp;
+            }
+        }
+
+        printf("%d\n", (first - last) * 2);
+    }
+    return 0;
+}
