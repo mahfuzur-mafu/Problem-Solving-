658650b8d1a849a88f9c9743e1a80fa25c2e4a32
Md Mahfuzur Rahman <63856744+mahfuzur-mafu@users.noreply.github.com>
2021-02-01 19:35:04 +0600

    Add files via upload

---
+++ Virtual Judge Solution/Games.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int a[100];
+    int b[100];
+    int count=0;
+    scanf("%d",&n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d %d",&a[i],&b[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (a[i] == b[j])
+                count++;
+        }
+
+    }
+    printf("%d\n",count);
+}
