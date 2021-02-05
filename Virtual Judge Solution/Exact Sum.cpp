8ba0bb9c383fa11032017d139c4ecc8fb0293778
Md Mahfuzur Rahman <63856744+mahfuzur-mafu@users.noreply.github.com>
2021-02-05 19:53:38 +0600

    Add files via upload

---
+++ Virtual Judge Solution/Exact Sum.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int n,c,d,tk,x,y,book1,book2,i,j;
+    int swap;
+    while(scanf("%d",&n)!=EOF)
+    {
+        int array[n];
+        for(i=0; i<n; i++)
+        {
+            scanf("%d",&array[i]);
+        }
+        sort(array,array+n);
+        scanf("%d",&tk);
+
+        for( i=0; i<n; i++)
+        {
+            for( j=i; j<n; j++)
+            {
+                x = array[i];
+                y = array[j];
+                if(x+y==tk)
+                {
+                    book1=x;
+                    book2=y;
+                }
+            }
+        }
+        printf("Peter should buy books whose prices are %d and %d.\n\n",book1,book2);
+    }
+}
+
+
