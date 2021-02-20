8771e21b072a5c2c83f2da070990cc4ee6109185
Md Mahfuzur Rahman <63856744+mahfuzur-mafu@users.noreply.github.com>
2021-02-20 19:00:43 +0600

    Add files via upload

--- Virtual Judge Solution/Polycarp Training.c
+++ Virtual Judge Solution/Polycarp Training.c
@@ -1,43 +1,80 @@
+
 #include <stdio.h>
+
+void msort(int ar[],int l,int m,int r)
+{
+    int left=m-l+1;
+    int right=r-m;
+    int leftar[left+1],rightar[right+1];
+
+    for(int i=l,j=0; i<m+1; i++,j++)
+    {
+
+        leftar[j]=ar[i];
+    }
+
+    for(int i=m+1,j=0; i<=r; i++,j++)
+    {
+        rightar[j]=ar[i];
+    }
+    int i=0,j=0,k=l;
+
+    while(i<left&&j<right)
+    {
+        if(leftar[i]<rightar[j])
+        {
+            ar[k]=leftar[i];
+            i++;
+        }
+        else
+        {
+            ar[k]=rightar[j];
+            j++;
+        }
+        k++;
+    }
+    while(j<right)
+    {
+        ar[k++]=rightar[j++];
+    }
+    while(i<left)
+    {
+        ar[k++]=leftar[i++];
+    }
+
+}
+void merges(int arr[],int l,int h)
+{
+    if(l>=h)
+    {
+        return  ;
+    }
+    int m=(l+h)/2;
+    merges(arr,l,m);
+    merges(arr,m+1,h);
+    msort(arr,l,m,h);
+
+}
 int main()
 {
-
-    int n;
-    long long int  array[100];
-    int i,c,d,swap;
-    int minvalue,loop,j;
+    int n,i;
     scanf("%d",&n);
-    for (i = 0; i < n; i++)
+    int ar[n];
+    for(int i=0; i<n; i++)
     {
-        scanf("%lld", &array[i]);
-    }
+        scanf("%d",&ar[i]);
 
-    for(loop = 0 ; loop < n-1 ; loop++)
-    {
-        minvalue = loop;
-        for(j = loop + 1 ; j < n ; j++)
-        {
-            if(array[minvalue] > array[j])
-            {
-                minvalue = j;
-            }
-        }
-        if(minvalue != loop)
-        {
-            swap = array[loop];
-            array[loop] = array[minvalue];
-            array[minvalue] = swap;
-        }
     }
-
+    merges(ar,0,n-1);
     long long int  prb=1;
     for(i=0; i< n; i++)
     {
-        if(array[i]>=prb)
+        if(ar[i]>=prb)
         {
             prb++;
         }
     }
     printf("%lld\n",prb-1);
-
+    return 0;
 }
+
