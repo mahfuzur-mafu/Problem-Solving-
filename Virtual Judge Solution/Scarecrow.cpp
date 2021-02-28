4f02648462b4ea00ba9956fe5223a6a33548eb16
Md Mahfuzur Rahman <63856744+mahfuzur-mafu@users.noreply.github.com>
2021-02-28 18:46:39 +0600

    Add files via upload

---
+++ Virtual Judge Solution/Scarecrow.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int n;
+    int i,j;
+    scanf("%d", &n);
+    for(i = 0; i <n; i++)
+    {
+        int m;
+        scanf("%d",&m);
+        char str[m];
+        scanf("%s",str);
+        int sum=0;
+        for(j=0; j<m;)
+        {
+            if (str[j] == '.')
+            {
+                sum++;
+                j=j+3;
+            }
+            else
+              j++;
+        }
+        printf("Case %d: %d\n",i+1,sum);
+    }
+}
+
