e4630f44e6514b829fea8e964b62281347d3dd10
Md Mahfuzur Rahman <63856744+mahfuzur-mafu@users.noreply.github.com>
2021-03-01 22:34:11 +0600

    Add files via upload

---
+++ Virtual Judge Solution/A Match Making Problem.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,m,i;
+    int tc=1;
+    int ar[10010],arr[10010];
+    while(scanf("%d %d",&n,&m)!=EOF)
+    {
+        if(n == 0 && m == 0)
+        {
+            break;
+        }
+
+        for( i = 0; i < n; i++)
+        {
+            scanf("%d",&ar[i]);
+        }
+        int min=ar[0];
+        for( i=1; i<n; i++)
+        {
+            if(min>ar[i])
+            {
+                min=ar[i];
+            }
+
+        }
+        for( i = 0; i < m; i++)
+        {
+            scanf("%d",&arr[i]);
+        }
+
+        if(n-m<=0)
+        {
+            printf("Case %d: 0\n",tc++);
+
+        }
+        else
+        {
+         printf("Case %d: %d %d\n",tc++,n-m, min);
+        }
+    }
+}
