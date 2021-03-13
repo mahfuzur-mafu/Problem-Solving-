483d67f7f09aa30744e8456f73dd67721f55095a
Md Mahfuzur Rahman <63856744+mahfuzur-mafu@users.noreply.github.com>
2021-03-13 22:03:11 +0600

    Add files via upload

---
+++ Virtual Judge Solution/Counting Kangaroos is Fun.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+void msort(int ar[],int l,int m,int r)
+{
+    int left=m-l+1;
+    int right=r-m;
+    int leftar[left+1],rightar[right+1];
+
+    for(int i=l,j=0; i<m+1; i++,j++)
+    {
+
+        leftar[j]=ar[i];
+    }
+
+    for(int i=m+1,j=0; i<=r; i++,j++)
+    {
+        rightar[j]=ar[i];
+    }
+    int i=0,j=0,k=l;
+
+    while(i<left&&j<right)
+    {
+        if(leftar[i]<rightar[j])
+        {
+            ar[k]=leftar[i];
+            i++;
+        }
+        else
+        {
+            ar[k]=rightar[j];
+            j++;
+        }
+        k++;
+    }
+    while(j<right)
+    {
+        ar[k++]=rightar[j++];
+    }
+    while(i<left)
+    {
+        ar[k++]=leftar[i++];
+    }
+
+}
+void merges(int arr[],int l,int h)
+{
+    if(l>=h)
+    {
+        return  ;
+    }
+    int m=(l+h)/2;
+    merges(arr,l,m);
+    merges(arr,m+1,h);
+    msort(arr,l,m,h);
+
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int ar[n];
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d",&ar[i]);
+
+    }
+    merges(ar,0,n-1);
+    int res=n-1;
+    for(int i=n/2-1; i>=0; i--)
+    {
+        if(ar[i]*2<=ar[res])
+        {
+            res--;
+        }
+    }
+    printf("%d\n",res+1);
+
+    return 0;
+}
+
